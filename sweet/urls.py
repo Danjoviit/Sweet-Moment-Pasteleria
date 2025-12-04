9ca06ce7b3a2e7e5cde2c5ebabc094f9b7b90644
Danjoviit <danjovitvilla@gmail.com>
2025-12-03 20:23:27 -0400

    creacion de endpoint para metodos crud por id en categorias

--- sweet/urls.py
+++ sweet/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Categoria_list
+from .views import Categoria_list, Categoria_detail
 
 
 urlpatterns = [
     path('categories/', Categoria_list, name='categoria-list'),
+    path('categories/<int:pk>/', Categoria_detail, name='categoria-detail'),
 ]
