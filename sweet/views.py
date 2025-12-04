8cedc45a3a3328d664c4f65fa39cf3f9e1a89c68
Danjoviit <danjovitvilla@gmail.com>
2025-12-04 14:28:20 -0400

    creacion de vista para los usuarios, registro, inicio, cambio de password, logout

--- sweet/views.py
+++ sweet/views.py
@@ -1,10 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
-from .serializers import CategoriaSerializer, ProductoSerializer, OrderSerializer
+from .serializers import CategoriaSerializer, ProductoSerializer, OrderSerializer, UserSerializers, RegisterSerializer, CustomLoginSerializer
 from .models import *
 from django.shortcuts import get_object_or_404
-# Create your views here.
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+
+
 
 
 @api_view(['GET', 'POST'])
@@ -148,3 +154,68 @@ def order_status_update(request, pk):
             serializers.save()
             return Response(serializers.data)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['POST'])
+def register_user(request):
+    Permission_classes = [AllowAny]
+    serializers = RegisterSerializer(data=request.data)
+    if serializers.is_valid():
+        user = serializers.save()
+        return Response({
+            "mesagge": "Usuario creado existosamente",
+            "user": UserSerializers(user).data
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login_user(request):
+    permission_classes = [AllowAny]
+    serializer = CustomLoginSerializer(data=request.data)
+
+    if serializer.is_valid():
+        email = serializer.validate_data['email']
+        password = serializer.validated_data['password']
+
+        #AUTENTICACION MANUAL
+
+        user = authenticate(username=email, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                'token': str(refresh.access_token),
+                'refresh': str(refresh),
+                'user': UserSerializers(user).data
+            }, status=status.HTTP_200_OK)
+        return Response({'error': 'Credenciales invalidas'}, status=status.HTTP_401_UNAUTHORIZED)
+    return Response(serializer.errores, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_current_user(request):
+    permission_classes = [IsAuthenticated]
+    serializer = UserSerializers(request.user)
+    return Response({'user': serializer.data})
+
+@api_view(['PATCH'])
+def update_profile(request):
+    permission_classes = [IsAuthenticated]
+    user = request.user
+    serializer = UserSerializers(user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def logout_user(request):
+    try:
+        refresh_token = request.data.get("refresh_token")
+        if refresh_token:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        return Response({"message": "logout exitoso"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response ({"error": 'Token invalido'}, status=status.HTTP_400_BAD_REQUEST)
